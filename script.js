a9a2f6d653659b5defedaabe7863ee224681ccd0
nkalinich <nataliekalinich@gmail.com>
2022-05-28 16:02:20 +0300

    cylinder volume

---
+++ script.js
@@ -0,0 +1,20 @@
+S = Math.PI;
+H = 20;
+R = 10;
+V = S * Math.pow(10, 2) * H;
+
+line = "--------------------";
+
+a = "Объем цилиндра с площадью основы S =";
+b = "радиусом R =";
+c = "и высотой H =";
+d = "равен:";
+e = "V = ";
+f = "end.";
+
+console.log("********************");
+console.log(a, S, b, R, c, H, d );
+console.log(line);
+console.log(e, V);
+console.log(line);
+console.log(f);
